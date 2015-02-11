d17cf1e7bf533343f07cb707dc3a118a77352b14
Anomitra Saha <anomitra1993@gmail.com>
2015-02-11 07:02:48 +0000

    [FEATURE] Parses both the innings.
    *Parses data for both batting innings, stores in the player_dict dictionary

--- scoresheet_generator.py
+++ scoresheet_generator.py
@@ -22,43 +22,41 @@ player_dict={"Rohit Sharma":0}
 html = crawl(base_url)
 print "Page crawled."
 parsed_html = BeautifulSoup(html)
-bat_table=parsed_html.find("table",{ "class" : "batting-table innings" })
-rows=bat_table.findAll("tr",{"class":None})
-
-
-
-# BATTING TABLE PARSER
-
-
-for row in rows:
-	name=""
-	batScore=0
-	runs=0
-	fields=row.findAll("td")
-	for i in range(0,8):
-		if(i==1):
-			left="view the player profile for "
-			right="\" "
-			name = re.search('%s(.*)%s' % (left, right), str(fields[i])).group(1)
-			print name,
-		if(i>=2 and i<=8):
-			left=">"
-			right="</td>"
-			result = re.search('%s(.*)%s' % (left, right), str(fields[i])).group(1)
-			#print result,
-			if(i==3):
-				runs=int(result.strip())
-				if(runs==0):
-					batScore=batScore-5 #IMPACT DUCK
-				batScore=batScore+runs #RUNS SCORE
-				batScore=batScore+(runs/25)*10 #MILESTONE BONUS
-			if(i==5):
-				batScore=batScore+runs-int(result.strip()) #PACE BONUS
-			if(i==7):
-				batScore=batScore+int(result.strip())*2 #IMPACT SIXES
-	#print"  BATSCORE:",batScore
-	print ""
-	player_dict[name]=batScore
+bat_table=parsed_html.findAll("table",{ "class" : "batting-table innings" })
+for table in bat_table:
+	rows=table.findAll("tr",{"class":None})
+	
+	# BATTING TABLE PARSER FOR BOTH INNINGS
+	
+	for row in rows:
+		name=""
+		batScore=0
+		runs=0
+		fields=row.findAll("td")
+		for i in range(0,8):
+			if(i==1):
+				left="view the player profile for "
+				right="\" "
+				name = re.search('%s(.*)%s' % (left, right), str(fields[i])).group(1)
+				print name,
+			if(i>=2 and i<=8):
+				left=">"
+				right="</td>"
+				result = re.search('%s(.*)%s' % (left, right), str(fields[i])).group(1)
+				#print result,
+				if(i==3):
+					runs=int(result.strip())
+					if(runs==0):
+						batScore=batScore-5 #IMPACT DUCK
+					batScore=batScore+runs #RUNS SCORE
+					batScore=batScore+(runs/25)*10 #MILESTONE BONUS
+				if(i==5):
+					batScore=batScore+runs-int(result.strip()) #PACE BONUS
+				if(i==7):
+					batScore=batScore+int(result.strip())*2 #IMPACT SIXES
+		#print"  BATSCORE:",batScore
+		print ""
+		player_dict[name]=batScore
 print player_dict
 #print rows[1].findAll("td")
 #print bat_table
